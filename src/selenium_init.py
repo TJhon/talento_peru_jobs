3688d2eeb38d604caa737bfbd2722dd0ab870d02
jhonkFr <jhonk.frz@gmail.com>
2024-08-04 22:26:15 -0500

    data

--- src/selenium_init.py
+++ src/selenium_init.py
@@ -14,30 +14,36 @@ from utils import query_success
 from navigation_pages import navigate_to, page_num
 
 
-driver = webdriver.Chrome()
-driver.get(
-    "https://app.servir.gob.pe/DifusionOfertasExterno/faces/consultas/ofertas_laborales.xhtml"
-)
+import multiprocessing
 
-time.sleep(3)
-print("complete loading")
 
-location = filter_region(driver, 5)
-query_success(driver)
+def scrapper(options=None, n_reg=1):
 
-total_positions_in_page = get_n_positions(driver)
-begin_page, total_pages = page_num(driver)
+    if options is not None:
+        driver = webdriver.Chrome(options=options)
+    else:
+        driver = webdriver.Chrome()
+    driver.get(
+        "https://app.servir.gob.pe/DifusionOfertasExterno/faces/consultas/ofertas_laborales.xhtml"
+    )
 
-data = get_info_page(driver, total_positions_in_page)
+    time.sleep(3)
+    print("complete loading")
 
-while begin_page < total_pages:
-    begin_page, total_pages = navigate_to(driver)
+    location = filter_region(driver, n_reg)
+    query_success(driver)
+
     total_positions_in_page = get_n_positions(driver)
-    result_page = get_info_page(driver, total_positions_in_page)
-    data = pd.concat([data, result_page])
-    # total1.append(total2)
-    print(begin_page, total_pages)
-import pandas as pd
+    begin_page, total_pages = page_num(driver)
 
-data = pd.DataFrame(data)
-data.to_csv(f"./data/{location}.csv", index=False)
+    data = get_info_page(driver, total_positions_in_page)
+
+    while begin_page < total_pages:
+        begin_page, total_pages = navigate_to(driver)
+        total_positions_in_page = get_n_positions(driver)
+        result_page = get_info_page(driver, total_positions_in_page)
+        data = pd.concat([data, result_page])
+        print(begin_page, total_pages)
+
+    data = pd.DataFrame(data)
+    data.to_csv(f"./data/{location}.csv", index=False)
--- src/utils.py
+++ src/utils.py
@@ -3,31 +3,37 @@ import time
 
 # pop up para cuando la pagina se cargo correctamente
 loading_success = '//*[@id="frmLstOfertsLabo:mensaje_container"]/div/div/div[2]/p'
-loading_page = 'j_idt85'
+loading_page = "j_idt85"
+
 
 def query_success(driver: WebDriver):
     """
-    Verifica que si despues de aplicar los filtros se 
+    Verifica que si despues de aplicar los filtros se
     carga los elementos correctos y da 1 segundo adicional
     para poder cargar
     """
     time.sleep(3)
+
     def success():
         find = driver.find_elements(By.XPATH, loading_success)
-        return find, len(find) > 0 #encontro el elemento
+        return find, len(find) > 0  # encontro el elemento
+
     find, popup = success()
     while not popup:
-        print('loading', popup)
+        time.sleep(1)
+        print("loading", popup)
         find, popup = success()
     print(find[0].text)
-    time.sleep(1) 
+    time.sleep(1)
+
 
 def change_page_loading(driver: WebDriver):
     def get_loading():
         div1 = driver.find_element(By.ID, loading_page)
-        style = div1.get_attribute('style')
+        style = div1.get_attribute("style")
         loading = "display: block" in style
-        return loading 
+        return loading
+
     loading = get_loading()
     while loading:
         time.sleep(0.1)
