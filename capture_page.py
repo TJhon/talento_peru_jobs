d33152e3c2d64b4790030ebbfa9f29ab757d56fd
Jhon Flores Rojas <48776907+TJhon@users.noreply.github.com>
2024-09-06 12:21:45 -0500

    Update capture_page.py

--- capture_page.py
+++ capture_page.py
@@ -1,20 +1,19 @@
 import time
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
-from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.firefox.service import Service
+from selenium.webdriver.firefox.options import Options
+from webdriver_manager.firefox import GeckoDriverManager
 
-# Configurar opciones de Chrome
-chrome_options = Options()
-chrome_options.add_argument("--no-sandbox")
-chrome_options.add_argument("--disable-dev-shm-usage")  # Opcional: Para entornos con poca memoria compartida
-chrome_options.add_argument("--headless")
-chrome_options.add_argument(
-    "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+# Configurar opciones de Firefox
+firefox_options = Options()
+firefox_options.add_argument("--headless")
+firefox_options.add_argument("--disable-dev-shm-usage")  # Opcional: Para entornos con poca memoria compartida
+firefox_options.add_argument(
+    "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:91.0) Gecko/20100101 Firefox/91.0"
 )
-chrome_options.add_argument("--disable-blink-features=AutomationControlled")
-# Inicializar Selenium con Chrome
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+
+# Inicializar Selenium con Firefox
+driver = webdriver.Firefox(service=Service(GeckoDriverManager().install()), options=firefox_options)
 
 # Navegar a la página
 driver.get("https://app.servir.gob.pe/DifusionOfertasExterno/faces/consultas/ofertas_laborales.xhtml")
@@ -28,6 +27,3 @@ driver.save_screenshot("captura_pagina.png")
 
 # Cerrar el navegador
 driver.quit()
-
-
-
