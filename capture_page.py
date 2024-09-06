d4f5202442cf5e4a2b179ac445a49b228cf26929
Jhon Flores Rojas <48776907+TJhon@users.noreply.github.com>
2024-09-06 12:17:18 -0500

    Update capture_page.py

--- capture_page.py
+++ capture_page.py
@@ -9,7 +9,10 @@ chrome_options = Options()
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")  # Opcional: Para entornos con poca memoria compartida
 chrome_options.add_argument("--headless")
-
+chrome_options.add_argument(
+    "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+)
+chrome_options.add_argument("--disable-blink-features=AutomationControlled")
 # Inicializar Selenium con Chrome
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
 
