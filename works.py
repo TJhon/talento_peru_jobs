fa15f36f8a2312cb2978e5f8a281d189f89f9617
Jhon Flores Rojas <48776907+TJhon@users.noreply.github.com>
2024-09-06 10:38:52 -0500

    Update works.py

--- works.py
+++ works.py
@@ -12,17 +12,8 @@ options.add_argument("--disable-dev-shm-usage")
 
 
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "--n_reg", type=int, required=True, help="Peru-Region number 1-24"
-    )
-    parser.add_argument("--local", type=int, required=True, help="Run locally")
 
-    args = parser.parse_args()
-    if args.local == 1:
-        scrapper(n_reg=args.n_reg)
-    else:
-        scrapper(n_reg=args.n_reg, options=options, github=True)
+    scrapper(n_reg=12, options=options, github=True)
 
 
 if __name__ == "__main__":
