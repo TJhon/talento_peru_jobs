f75a5389303a7bf135a4efd11cba2a6db6a17cea
tjhon <fr.jhonk@gmail.com>
2024-09-24 16:22:38 -0500

    Data worker 1 Tue 09/24/2024

--- scrapper_works_worker1.bat
+++ scrapper_works_worker1.bat
@@ -1,2 +1,6 @@
 @echo off
 python works.py --n_reg 1 2 3 4 5 6 7 8 9 10 11 12
+set today=%date%
+git add -A
+git commit -m "Data worker 1 %today%"
+git push -u origin testing_page
--- scrapper_works_worker2.bat
+++ scrapper_works_worker2.bat
@@ -1,2 +1,6 @@
 @echo off
 python works.py --n_reg 12 13 14 15 16 17 18 19 20 21 22 23 24 25
+set today=%date%
+git add -A
+git commit -m "Data worker 2 %today%"
+git push -u origin testing_page
